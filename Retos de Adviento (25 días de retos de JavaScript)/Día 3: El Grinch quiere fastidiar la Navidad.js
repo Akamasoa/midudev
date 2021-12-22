bad05d1d2c764364648ae50a7d8b76afe0da0af5
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-12-21 23:00:13 -0300

    Update Día 3: El Grinch quiere fastidiar la Navidad.js

--- Retos de Adviento (25 días de retos de JavaScript)/Día 3: El Grinch quiere fastidiar la Navidad.js
+++ Retos de Adviento (25 días de retos de JavaScript)/Día 3: El Grinch quiere fastidiar la Navidad.js
@@ -22,3 +22,29 @@ Ejemplos:
 Crea una función que pasándole el texto de la carta, devuelva true si es válida y false si no lo es. ¡Y acaba con la travesura del Grinch!
 */
 
+export default function isValid(letter) {
+ // ¡No dejes que el Grinch gane!
+ const open = [...letter].filter((letra) => letra === "(").length
+  const close = [...letter].filter((letra) => letra === ")").length
+  const vacio = letter.replace('()',"").trim();
+  const llave = [...letter].filter((letra) => letra === "{").length
+  const corchete = [...letter].filter((letra) => letra === "[").length
+
+  if(
+    (letter.length > vacio.length)
+    || letter[0] === ")"
+    || (open !== close)
+    || (llave || corchete)){
+    return false;
+  }
+  return true;
+}
+
+
+console.log(isValid("bici coche (balón) bici coche peluche"))
+console.log(isValid("(muñeca) consola bici"))
+console.log(isValid("bici coche (balón bici coche"))
+console.log(isValid("peluche (bici [coche) bici coche balón"))
+console.log(isValid("(peluche {) bici"))
+console.log(isValid("() bici"))
+console.log(isValid(")bici( casa play"))
